2080292cf7a4d6edae130b118c3d6c6793a932a9
cenkbcark <79763515+cenkbcark@users.noreply.github.com>
2022-09-17 14:12:30 +0300

    Initial Commit

---
+++ MovieDatabaseAPPUITests/MovieDatabaseAPPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDatabaseAPPUITestsLaunchTests.swift
+//  MovieDatabaseAPPUITests
+//
+//  Created by Cenk Bahadır Çark on 17.09.2022.
+//
+
+import XCTest
+
+class MovieDatabaseAPPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
